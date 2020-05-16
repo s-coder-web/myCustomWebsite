13c8f9c0ca1fa38b745b68d5d900da87f86fa020
s-coder-web <65434170+s-coder-web@users.noreply.github.com>
2020-05-16 08:57:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 window.onload = function() {
   c = document.getElementById("canvas");
   cc = c.getContext("2d");
+  setInterval(update, 1000/30);
+}
+
+function update() {
+  cc.fillStyle = "#fff";
 }

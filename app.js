49a2595bfba65fb8a7e2054b2e1b0ae2bcfbb503
s-coder-web <65434170+s-coder-web@users.noreply.github.com>
2020-05-16 09:01:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   c = document.getElementById("canvas");
-  cc = c.getContext("webgl");
+  gl = c.getContext("webgl");
   setInterval(update, 1000/30);
 }
 
 function update() {
-  
+
 }
